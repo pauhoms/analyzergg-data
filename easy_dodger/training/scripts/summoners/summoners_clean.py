c4eb521b519a826187ccf2d86ba3e7eb72836f48
pauhoms <pauhomsjimenez1@gmail.com>
2020-09-14 20:38:41 +0200

    refactor: create a function for get all summoners from one division

--- easy_dodger/training/scripts/summoners/summoners_clean.py
+++ easy_dodger/training/scripts/summoners/summoners_clean.py
@@ -14,28 +14,38 @@ def _init():
 
 
 def _get_all_summoners() -> pd.DataFrame:
-    all_players = pd.DataFrame()
+    all_summoners = pd.DataFrame()
     for tier in _ALL_TIERS:
         for division in _ALL_DIVISIONS:
-
-            get_all_players_from_that_division = \
-                "https://euw1.api.riotgames.com/lol/league/v4/entries/"
-
-            get_all_players_from_that_division = \
-                get_all_players_from_that_division + \
-                "RANKED_SOLO_5x5/"+tier+"/"+division+"?api_key=" + RIOT_API_KEY
-
-            request = _HTTP.request(
-                    'GET',
-                    get_all_players_from_that_division
+            all_summoners = _get_summoners_from_that_division(
+                    tier,
+                    division,
+                    all_summoners
                 )
 
-            all_players = pd.concat([
-                pd.read_json(request.data),
-                all_players
-            ])
+    return all_summoners.summonerName
 
-    return all_players.summonerName
+
+def _get_summoners_from_that_division(tier, division, all_summoners):
+
+    get_all_players_from_that_division = \
+        "https://euw1.api.riotgames.com/lol/league/v4/entries/"
+
+    get_all_players_from_that_division = \
+        get_all_players_from_that_division + \
+        "RANKED_SOLO_5x5/"+tier+"/"+division+"?api_key=" + RIOT_API_KEY
+
+    request = _HTTP.request(
+            'GET',
+            get_all_players_from_that_division
+        )
+
+    all_summoners = pd.concat([
+            pd.read_json(request.data),
+            all_summoners
+        ])
+
+    return all_summoners
 
 
 def _generate_file(players) -> None:
